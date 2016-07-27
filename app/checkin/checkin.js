7e6a93f2fc238a30397fa4aa511ae246f77052e7
Machinewarlock <jameschu1874@gmail.com>
2016-07-27 16:31:30 +0100

    Fixed
    Checkin goes straight to onboarding

--- app/checkin/checkin.js
+++ app/checkin/checkin.js
@@ -29,7 +29,7 @@ function processCheckin() {
       // Our logic. If the distance between our user and the target is less than 100m, show the login screen, else update the view with a helpful message
       if (distance < 1000) {
 
-      finishedCurrentView();
+      window.location.href = '/app/onboarding/';
 
       } else {
         $('p.geolocation-message').html('Please try again when you get to the Southbank Centre');
