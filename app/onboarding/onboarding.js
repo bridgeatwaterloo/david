63dc29def5c1ec3e3ff5424d624997973e417e07
Kellyana <thekellyana@gmail.com>
2016-07-27 17:00:54 +0100

    working completely

--- app/onboarding/onboarding.js
+++ app/onboarding/onboarding.js
@@ -4,18 +4,43 @@ function showDiv(divName) {
 }
 
 // When the user clicks the button
-$('.btn-username').click(function(){
+$('.btn-username').click(processForm, function(){
 	showDiv(".prep");
-
+	$('.btn-all').hide();
+	setTimeout(function() {$('.btn-prep').show();},10000);
+	
 });
 
 // When the user clicks the button
 $('.btn-prep').click(function(){
 	showDiv(".instruction");
-
+	setTimeout(function() {$('.btn-instruction').show();},10000);
 })
 
-// When the user clicks the button
-//$('.btn-instruction').click(showDiv);
+// Get the form, listen to its submit event
+
+$('.form-set-username').on('submit', processForm);
+
+var currentView = window.location.pathname;
+setView(currentView);
+
+// function to run when the form is submitted
+function processForm() {
+
+	showDiv(".prep");
+	$('.btn-all').hide();
+	setTimeout(function() {$('.btn-prep').show();},10000);
+ // get the entered user name from the input
+ var username = $('input').val();
+ localStorage.setItem('username', username);
+   database.ref('users/'+ username).set({
+     username: username
+    });
+
+
+ 
+ return false;
+
+};
 
 showDiv(".username")
