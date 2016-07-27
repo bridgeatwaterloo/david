820dd884335876985b8f109fed3a3b7d221419d9
kimberleynyam <kimberleynyam@hotmail.co.uk>
2016-07-27 11:20:54 +0100

    Heart monitor line
    Not completely finished - need to work on/play with speed etc

--- projections/projections-global-js/projections.js
+++ projections/projections-global-js/projections.js
@@ -1,3 +1,42 @@
 
 
-showView('.view-decision-result');
+showView('.heartbeat');
+
+var speed = 4000;
+
+setTimeout(function(){
+	speed = 8000;
+	setTimeout(function(){
+		speed = 2000;
+	}, 5000);
+}, 5000);
+
+
+function animateRight() {
+$(".heartbeat-right-overlay").animate({
+    width: "0%"
+  }, speed, function() {
+    // Animation complete.
+    $(".heartbeat-right-overlay").css({
+    	width: "90%"
+    });
+    animateRight();
+  });
+}
+
+animateRight()
+
+function animateLeft() {
+$(".heartbeat-left-overlay").animate({
+    width: "90%"
+  }, speed, function() {
+    // Animation complete.
+    $(".heartbeat-left-overlay").css({
+    	width: "0%"
+    });
+    animateLeft();
+  });
+}
+
+animateLeft()
+  
