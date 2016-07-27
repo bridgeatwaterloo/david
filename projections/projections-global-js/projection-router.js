7811f22fc75182086e6087dcbcd0f7d128c14174
francis <francis.smedley@hotmail.co.uk>
2016-07-27 16:44:35 +0100

    added a database listener for decision-are-you-happy

--- projections/projections-global-js/projection-router.js
+++ projections/projections-global-js/projection-router.js
@@ -2,88 +2,123 @@
 firebase.database().ref('view').on('value', function(snapshot) {
 	view = snapshot.val().currentView;
 	  switch(view) {
-	        case"/app/checkin/" :
-	        		alert("checkin");
-	        		//code block
-	        		break;
-		    case"/app/onboarding/" :
-		    		//code block
-		    		alert("username");
-		    		break;
-		    case"/app/decision-are-you-happy/" :
-		    		//code block
-		    		break;
-		    case"/app/davidToLife/" :
-		    		//code block
-		    		break;
-		    case"/app/decision-where-david/" :
-		    		//code block
-		    		break;
-		    case"/app/decision-reason-for-david-to-move/" :
-		    		//code block
-		    		break;
-		    case"/app/warning/" :
-		    		//code block
-		    		break;
-		    case"/app/decision-clothing/" :
-		    		//code block
-		    		break;
-		    case"/app/decision-emotion/" :
-		    		//code block
-		    		break;
-		    case"/app/slider-horizontal/" :
-		    		//code block
-		    		break;
-		    case"/app/slider-vertical/" :
-		    		//code block
-		    		break;
-		    case"/app/calculating-results/" :
-		    		//code block
-		    		break;
-		    case"/app/done/" :
-		    		//code block
-		    		break;
-		    case"/app/percentage-of-vote/" :
-		    		//code block
-		    		break;
-		    case"/app/pay-attention/" :
-		    		//code block
-		    		break;
-		    case"/app/decision-want-more/" :
-		    		//code block
-		    		break;
-		    case"/app/time-left-with-david/" :
-		    		//code block
-		    		break;
-		    case"/app/decision-are-you-finished/" :
-		    		//code block
-		    		break;
-		    case"/app/review-david" :
-		    		//code block
-		    		break;
-		    case"/app/review-how-was-david/" :
-		    		//code block
-		    		break;
-		    case"/app/review-would-you-see-david-again/" :
-		    		//code block
-		    		break;
-		    case"/app/review-thank-you/" :
-		    		//code block
-		    		break;
-		    default:
+	       case "/app/checkin/" :
+	       		//code block
+	       		showView('.checkin');
+	       		break;
+		   case "/app/onboarding/" :
+		   		//code block
+		   		showView('.onboarding');
+		   		break;
+		   case "/app/decision-are-you-happy/" :
+		   		showView('.decision-are-you-happy');
+		   		//code block
+		   		break;
+		   case "/app/davidToLife/" :
+		   		showView('.davidToLife');
+		   		//code block
+		   		break;
+		   case "/app/decision-where-david/" :
+		   		showView('.decision-where-david');
+		   		//code block
+		   		break;
+		   case "/app/decision-reason-for-david-to-move/" :
+		   		showView('.decision-reason-for-david-to-move');
+		   		//code block
+		   		break;
+		   case "/app/warning/" :
+		   		showView('.warning');
+		   		//code block
+		   		break;
+		   case "/app/decision-clothing/" :
+		   		showView('.decision-clothing');
+		   		//code block
+		   		break;
+		   case "/app/decision-emotion/" :
+		   		showView('.decision-emotion');
+		   		//code block
+		   		break;
+		   case "/app/slider-horizontal/" :
+		   		showView('.slider-horizontal');
+		   		//code block
+		   		break;
+		   case "/app/slider-vertical/" :
+		   		showView('.slider-vertical');
+		   		//code block
+		   		break;
+		   case "/app/calculating-results/" :
+		   		showView('.calculating-results');
+		   		//code block
+		   		break;
+		   case "/app/done/" :
+		   		showView('.done');
+		   		//code block
+		   		break;
+		   case "/app/percentage-of-vote/" :
+		   		showView('.percentage-of-vote');
+		   		//code block
+		   		break;
+		   case "/app/pay-attention/" :
+		   		showView('.pay-attention');
+		   		//code block
+		   		break;
+		   case "/app/decision-want-more/" :
+		   		showView('.decision-want-more');
+		   		//code block
+		   		break;
+		   case "/app/time-left-with-david/" :
+		   		showView('.time-left-with-david');
+		   		//code block
+		   		break;
+		   case "/app/decision-are-you-finished/" :
+		   		showView('.decision-are-you-finished');
+		   		//code block
+		   		break;
+		   case "/app/review-david" :
+		   		showView('.review-david');
+		   		//code block
+		   		break;
+		   case "/app/review-how-was-david/" :
+		   		showView('.review-how-was-david');
+		   		//code block
+		   		break;
+		   case "/app/review-would-you-see-david-again/" :
+		   		showView('.review-would-you-see-david-again');
+		   		//code block
+		   		break;
+		   case "/app/review-thank-you/" :
+		   		showView('.review-thank-you');
+		   		//code block
+		   		break;
+			default:
 	 		
 	}
 });
 
 
-showView('.view-decision-result');
+firebase.database().ref('decisions').on('value', function(snapshot){
 
-function where-david(){
+	// console.log(snapshot.);
+	// console.log(snapshot.val());
+	// var data = snapshot.exportVal()
+	// console.log(data);
+	var numberOfNo=0;
+	var numberOfYes=0;
+	snapshot.forEach(function(data){
+		console.log(data.key + ": " +data.val() );
+		if(data.val()==="Yes"){
+			numberOfYes++;
+		}else{
+			numberOfNo++;
+		}
+	});
 
-	firebase.database().ref('view').on('value', function(snapshot) {
-
-
+	console.log("yes:" +numberOfYes);
+	if(numberOfYes>numberOfNo){
+		$(".decision-are-you-happy-result").html("YES");
+	}else{
+		$(".decision-are-you-happy-result").html("NO");
 	}
+});
 
 
-}
