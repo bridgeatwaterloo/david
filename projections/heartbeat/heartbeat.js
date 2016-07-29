72440ed2a08ca8b86fa35cba9ca5e152c6bdf61d
Marisa <marisaba2012@gmail.com>
2016-07-29 12:07:52 +0100

    david-to-life heartbeat projection

--- projections/heartbeat/heartbeat.js
+++ projections/heartbeat/heartbeat.js
@@ -1,42 +1,34 @@
 
+function startHeartbeat() {
 
-showView('.heartbeat');
+  var speed = 4000;
 
-var speed = 4000;
-
-setTimeout(function(){
-	speed = 8000;
-	setTimeout(function(){
-		speed = 2000;
-	}, 5000);
-}, 5000);
-
-
-function animateRight() {
-$(".heartbeat-right-overlay").animate({
-    width: "0%"
-  }, speed, function() {
-    // Animation complete.
-    $(".heartbeat-right-overlay").css({
-    	width: "90%"
+  function animateRight() {
+  $(".heartbeat-right-overlay").animate({
+      width: "0%"
+    }, speed, function() {
+      // Animation complete.
+      $(".heartbeat-right-overlay").css({
+      	width: "90%"
+      });
+      animateRight();
     });
-    animateRight();
-  });
-}
+  }
 
-animateRight()
+  animateRight()
 
-function animateLeft() {
-$(".heartbeat-left-overlay").animate({
-    width: "90%"
-  }, speed, function() {
-    // Animation complete.
-    $(".heartbeat-left-overlay").css({
-    	width: "0%"
+  function animateLeft() {
+  $(".heartbeat-left-overlay").animate({
+      width: "90%"
+    }, speed, function() {
+      // Animation complete.
+      $(".heartbeat-left-overlay").css({
+      	width: "0%"
+      });
+      animateLeft();
     });
-    animateLeft();
-  });
-}
+  }
 
-animateLeft()
+  animateLeft()
   
+}
