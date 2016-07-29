6f71a689a802a6816841086c5f7194cde18420a8
francis <francis.smedley@hotmail.co.uk>
2016-07-29 10:27:21 +0100

    Quick Fix
    a quick fix to the loading animation, only happens when the view is
    going to change, when view isn't ready to change extra text appears

--- app/holding/holding.js
+++ app/holding/holding.js
@@ -4,7 +4,6 @@ var nextview = getFireBaseView();
 var previousView = getView();
 
 function checkNextView() {
-   startLoadingAnimation()
   //get next view from firebase
   nextview = getFireBaseView();
   // get previous view from local storage
@@ -18,11 +17,15 @@ function checkNextView() {
   console.log(nextview, previousView);
 
   // if the next view is not the same as the previous view then go to the next view
-  setTimeout(function() {
+  
     if(previousView !== nextview){
-     
-      window.location.href = nextview;
+      startLoadingAnimation()
+      setTimeout(function() {
+        window.location.href = nextview;
+      }, 1000)
+    }else{
+      $('.extra-message').html(" The next view isn't ready yet, ")
     }
-  }, 1000)
+  
 
 }
