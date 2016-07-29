3eb6d13bc8ec15aacd8ce3aec5514234b393e1b1
francis <francis.smedley@hotmail.co.uk>
2016-07-29 13:56:36 +0100

    More listeners
    added more firebase listeners

--- projections/projections-global-js/firebase-listeners.js
+++ projections/projections-global-js/firebase-listeners.js
@@ -22,7 +22,7 @@ firebase.database().ref('decision-are-you-happy').on('value', function(snapshot)
 
 
 //Listener for the database for decision-clothing
-firebase.database().ref('clothing-decision').on('value', function(snapshot){
+firebase.database().ref('decision-clothing').on('value', function(snapshot){
 	var list ={
 		"Tie": 0,
 		"Shoes" : 0, 
@@ -59,3 +59,80 @@ firebase.database().ref('clothing-decision').on('value', function(snapshot){
 
 });
 
+//Listener for the decision-emotion page
+firebase.database().ref('decision-emotion').on('value', function(snapshot){
+	var list ={
+		"Unhappy": 0,
+		"Joyfull" : 0, 
+		"Needy" : 0,
+		"Frustrated" :0,
+		"Appathetic" :0
+	}
+
+	snapshot.forEach(function(data){
+		//console.log(data.key + ": " +data.val() );
+		if(data.val()==="Unhappy"){
+			list.Unhappy++;
+		}else if (data.val()==="Joyfull"){
+			list.Joyfull++;
+		}else if (data.val()==="Needy"){
+			list.Needy++;
+		}else if (data.val()=="Frustrated"){
+			list.Frustrated++;
+		}else if (data.val()=="Appathetic"){
+			list.Appathetic++;
+		}
+	});
+
+
+	var keyValues = [];
+
+	for(var key in list){
+		keyValues.push([key,list[key]])
+	}
+
+	keyValues.sort(function compare(kv1,kv2){
+		return kv1[1]-kv2[1];
+	});
+
+	//console.log(keyValues[3]);
+	$(".decision-emotion-result").html(keyValues[4][0]);
+
+});
+
+firebase.database().ref('decision-are-you-finished').on('value', function(snapshot){
+	var numberOfNo=0;
+	var numberOfYes=0;
+	snapshot.forEach(function(data){
+		//console.log(data.key + ": " +data.val() );
+		if(data.val()==="Yes"){
+			numberOfYes++;
+		}else{
+			numberOfNo++;
+		}
+	});
+
+	//console.log("yes:" +numberOfYes);
+	if(numberOfYes>numberOfNo){
+		$(".decision-are-you-finished-result").html("YES");
+	}else{
+		$(".decision-are-you-finished-result").html("NO");
+	}
+});
+
+firebase.database().ref('decision-where-david').on('value', function(snapshot){
+	var values = [];
+	snapshot.forEach(function(data){
+		//console.log(data.key + ": " +data.val() );
+		values.push([data.key,data.val()])
+	});
+	var length = values.length;
+	var random =  Math.floor((Math.random() * length-1) + 1);
+
+	console.log(values);
+	console.log(random);
+	var name = values[random][0];
+	var position = values[random][1]
+	$(".decision-where-david-name").html(name);
+	$(".decision-where-david-result").html(position);
+});
