967ca68fa513c8c48661f8368d940388b0d2ad95
MrPyroMeiniac <rich4rd.em4nuel@googlemail.com>
2016-08-05 11:54:04 +0100

    longi/latitude corrections

--- app/checkin/checkin.js
+++ app/checkin/checkin.js
@@ -17,16 +17,15 @@ function processCheckin() {
       var userLat = position.coords.latitude;
       var userLon = position.coords.longitude;
 
-      // The lat and long of St Andrew's Church.
-      // N.B. Must change this to Southbank centre before performance!
-      var targetLat = 51.5024578;
-      var targetLon = -0.1067309;
+      // The lat and long of Southbank centre
+      var targetLat = 51.505533; 
+      var targetLon = -0.117014;
 
       // calculate the distance by passing our variables to our helper function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) saved in helper-functions.js. The function returns a value in km that is the distance between two points. We assign this to a new variable, distance.
       var distance = getDistanceFromLatLonInKm(userLat, userLon, targetLat, targetLon);
 
       // Our logic. If the distance between our user and the target is less than 100m, show the login screen, else update the view with a helpful message
-      if (distance < 1000) {
+      if (distance < 100) {
         
          setTimeout(function() {
             window.location.href = '/app/onboarding/';
