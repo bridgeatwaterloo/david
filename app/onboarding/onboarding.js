7911dc0a81c9d9eee12910cceda07a8e02452191
pellensemble <pellensemble@gmail.com>
2016-08-05 17:26:17 +0100

    the code for anonymous

--- app/onboarding/onboarding.js
+++ app/onboarding/onboarding.js
@@ -32,6 +32,9 @@ function processForm() {
 	setTimeout(function() {$('.btn-prep').show();},10000);
  // get the entered user name from the input
 	var username = $('input').val();
+	if (username == null || username == ""){
+		username = "anonymous";
+	}
  	localStorage.setItem('username', username);
 		database.ref('users/'+ username).set({
     	username: username
