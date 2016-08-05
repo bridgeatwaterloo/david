a7a342cbb7b69a4692494b7a365ab9c1db0fd159
MrPyroMeiniac <rich4rd.em4nuel@googlemail.com>
2016-08-05 18:11:07 +0100

    speed up on the button

--- app/onboarding/onboarding.js
+++ app/onboarding/onboarding.js
@@ -7,21 +7,21 @@ function showDiv(divName) {
 $('.btn-username').click(processForm, function(){
 	showDiv(".prep");
 	$('.btn-all').hide();
-	setTimeout(function() {$('.btn-prep').show();},10000);
+	setTimeout(function() {$('.btn-prep').show();},3000);
 	
 });
 
 // When the user clicks the button
 $('.btn-prep').click(function(){
 	showDiv(".instruction");
-	setTimeout(function() {$('.btn-instruction').show();},10000);
+	setTimeout(function() {$('.btn-instruction').show();},3000);
 })
 
 // Get the form, listen to its submit event
 
 $('.form-set-username').on('submit', processForm);
 
-$('.btn-instruction').click(finishedOnboarding);
+$('.btn-instruction').click(finishedCurrentView);
 
 // function to run when the form is submitted
 function processForm() {
@@ -29,12 +29,9 @@ function processForm() {
 
 	showDiv(".prep");
 	$('.btn-all').hide();
-	setTimeout(function() {$('.btn-prep').show();},10000);
+	setTimeout(function() {$('.btn-prep').show();},3000);
  // get the entered user name from the input
 	var username = $('input').val();
-	if (username == null || username == ""){
-		username = "anonymous";
-	}
  	localStorage.setItem('username', username);
 		database.ref('users/'+ username).set({
     	username: username
