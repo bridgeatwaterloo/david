153bdbe1b5f3c7736bc9d74c56e41ac3b1a170ed
MrPyroMeiniac <rich4rd.em4nuel@googlemail.com>
2016-08-06 10:09:47 +0100

    music beats

---
+++ projections/heartbeat/heartpump.js
@@ -0,0 +1,10 @@
+var db1 = new audio (david-beat-one.mp3)
+var db2 = new audio (david-beat-two.mp3)
+
+function beatPump() {
+           if { ('.left-overlay') width == 25%, 47%;
+           db1.play;
+}
+           if { ('.right-overlay') width == 12%, 58%;
+           db2.play;
+}
