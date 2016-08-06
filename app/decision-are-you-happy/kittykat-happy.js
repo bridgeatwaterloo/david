9e8adfe181e3dd3e2c6d5d93c3e54ab8683b5ef9
MrPyroMeiniac <rich4rd.em4nuel@googlemail.com>
2016-08-06 11:20:15 +0100

    cat update two

--- app/decision-are-you-happy/kittykat-happy.js
+++ app/decision-are-you-happy/kittykat-happy.js
@@ -1,13 +1,7 @@
-var meow = new audio("cat.mp3")
+var meow = new audio("purrline.mp3")
 
 function kitty() {
 	if showIndicatorView = ('.decision-are-you-happy');
-		audio.currentTime = 0;
-		meow.play;
-		int = setInterval(function() {
-    if (audio.currentTime > 0.4) {
-    	audio.pause();
-   		clearInterval(int);
-		}
-	}, 20);
-}
+		meow.currentTime = 0;
+		meow.play();
+	}
