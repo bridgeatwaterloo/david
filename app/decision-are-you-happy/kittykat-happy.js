6b6b947538561e163296c801f44bc60010d15d39
MrPyroMeiniac <rich4rd.em4nuel@googlemail.com>
2016-08-06 11:10:32 +0100

    cat update

---
+++ app/decision-are-you-happy/kittykat-happy.js
@@ -0,0 +1,13 @@
+var meow = new audio("cat.mp3")
+
+function kitty() {
+	if showIndicatorView = ('.decision-are-you-happy');
+		audio.currentTime = 0;
+		meow.play;
+		int = setInterval(function() {
+    if (audio.currentTime > 0.4) {
+    	audio.pause();
+   		clearInterval(int);
+		}
+	}, 20);
+}
